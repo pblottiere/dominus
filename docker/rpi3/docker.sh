76189aeab4a8aa63b867f93ec86bc1e0e82a37b3
Blottiere Paul <blottiere.paul@gmail.com>
2018-03-04 13:26:46 +0100

    Add docker.sh to cross-compile on docker

---
+++ docker/rpi3/docker.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+IMAGE="dominus/rpi3"
+
+id() {
+  local id=$(docker inspect --format="{{.Id}}" $IMAGE)
+  echo "$id"
+}
+
+case "$1" in
+  build)
+    docker build . -t $IMAGE
+  ;;
+
+  configure)
+    docker run --rm -v $PWD/../..:/src -w /src -it $(id) ./autogen.sh
+    docker run --rm -v $PWD/../..:/src -w /src -it $(id) ./configure --host=arm-linux --prefix=/src/install
+  ;;
+
+  make)
+    docker run --rm -v $PWD/../..:/src -w /src -it $(id) make
+  ;;
+
+  install)
+    docker run --rm -v $PWD/../..:/src -w /src -it $(id) make install
+  ;;
+
+  maintainer-clean)
+    docker run --rm -v $PWD/../..:/src -w /src -it $(id) make maintainer-clean
+  ;;
+
+  clean)
+    docker rmi $(id)
+  ;;
+esac
+
+exit 0
