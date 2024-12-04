68d99b71bda2661d0174d91df0e8e128916d3939
Blottiere Paul <blottiere.paul@gmail.com>
2024-12-04 21:36:44 +0100

    Play christmas song

--- dominus-central/bsp/meta-dominus/recipes-dominus/dominusd/dominusd.bbappend
+++ dominus-central/bsp/meta-dominus/recipes-dominus/dominusd/dominusd.bbappend
@@ -1,10 +1,15 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
 
 SRC_URI += " file://dominusd.service"
+SRC_URI += " file://christmas0.wav"
 
 FILES:${PN} += "${sysconfdir}/systemd/system/dominusd.service"
+FILES:${PN} += "${datadir}/dominus/christmas0.wav"
 
 do_install:append() {
   install -d ${D}/${sysconfdir}/systemd/system
   install -m 0644 ${WORKDIR}/dominusd.service ${D}/${sysconfdir}/systemd/system/
+
+  install -d ${D}/${datadir}/dominus/
+  install -m 0644 ${WORKDIR}/christmas0.wav ${D}/${datadir}/dominus/
 }
--- dominus-central/src/dominusd.py
+++ dominus-central/src/dominusd.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 import time
+import subprocess
 import RPi.GPIO as GPIO
 
 PIN=36
@@ -7,10 +10,11 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def my_callback(channel):
-    print("DING DONG")
+    print("DING DONG", flush=True)
+    subprocess.run(["/usr/bin/aplay", "/usr/share/dominus/christmas0.wav"])
 
 GPIO.add_event_detect(PIN, GPIO.RISING, callback=my_callback, bouncetime=300)
 
-print("dominusd is starting...")
+print("dominusd is starting...", flush=True)
 while True:
     time.sleep(1)
